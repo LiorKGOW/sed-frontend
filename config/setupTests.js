a0e18b7a82d4d411999f3f9fd0ef8f2ccaa6ebae
Lior Keren <lkeren@redhat.com>
2024-01-25 19:19:42 +0200

    temp - self work

--- config/setupTests.js
+++ config/setupTests.js
@@ -1,8 +1,8 @@
-import { configure, mount, render, shallow } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// import { configure, mount, render, shallow } from 'enzyme';
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
 
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
 global.render = render;
